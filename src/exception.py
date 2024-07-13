74583d4b21d573613be91c2d88546db4827665b0
Anandhu Salimkumar <anandhusalimkumar05@gmail.com>
2024-07-13 21:21:47 +0530

    logging and exceptions

--- src/exception.py
+++ src/exception.py
@@ -8,5 +8,11 @@ def error_message_detail(error, exc_tb):
     error_message = f"Error occurred in Python script [{file_name}] at line number [{line_number}] with message: {str(error)}"
     return error_message
 
+class CustomException(Exception):
+    def __init__(self, error_message, exc_tb):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, exc_tb)
 
+    def __str__(self):
+        return self.error_message
 
